b88f769f01719c4ad8dec6f308098bf8265b09c4
Guilherme da Silva Boava <guiboava@hotmail.com>
2025-05-06 22:31:13 -0300

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'roll_the_sound',
+  webDir: 'www'
+};
+
+export default config;
